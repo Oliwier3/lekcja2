5d678217b5cce27e067fcfbc824104d443a5a00b
jp2137 <sbaiyassine2004@gmail.com>
2022-10-24 14:44:17 +0200

    2 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.superPrzycisk).setOnClickListener {
+            findViewById<Button>(R.id.superPrzycisk).text = "Dziala"
+        }
+        findViewById<Button>(R.id.piesPrzycisk).setOnClickListener {
+            findViewById<Button>(R.id.piesPrzycisk)
         }
     }
 }
